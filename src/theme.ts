36964a90239f8e2c38cc95cf155f0c85e28745cf
alinakassym <alinakassym@gmail.com>
2025-05-01 13:29:30 +0500

    feat(theme): add typography

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,20 @@ import { createTheme } from '@mui/material/styles'
 
 export const getTheme = (mode: 'light' | 'dark') =>
   createTheme({
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
     palette: {
       mode,
       primary: {
